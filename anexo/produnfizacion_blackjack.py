fdfbf8ac76056f8ed64223c692faf4a85245ef8d
LionelCastillo <105450241+LionelCastillo@users.noreply.github.com>
2022-07-16 20:00:12 -0300

    Add files via upload

--- anexo/produnfizacion_blackjack.py
+++ anexo/produnfizacion_blackjack.py
@@ -1,3 +1,7 @@
+from ast import While
+import random
+
+from pyparsing import White
 # Numpy [Python]
 # Ejercicios de profundización
 
@@ -35,11 +39,74 @@ BONUS Track: Realizar las modificaciones necesarias para que jueguen
 dos jugadores y compitan para ver quien sacá la suma de números
 más cercanos a 21 sin pasarse!
 '''
+def blackjack():
+    nombre = str(input('ingrese su nombre: '))
+
+    numeros = [random.randint(1,10) for x in range(2)]
+
+    while sum(numeros) < 21:
+        print(nombre, 'tus numeros son: ', numeros)
+        opcion = str(input('si desea recibir mas cartas ingrese SI en otro caso ingrese NO: '))
+        if opcion == 'SI':
+            numeros.append(random.randint(1,10))
+        elif opcion == 'NO': 
+            break
+        else:
+            print('elige una de las dos opciones, SI o NO') 
+
+    print(nombre, 'tus numeros son: ', numeros)
+
+    if sum(numeros) > 21:
+        print('Has perdido')
+        resultado = {'puntaje': 0, 'jugador': nombre}       
+        return resultado
+
+    resultado = {'puntaje': sum(numeros), 'jugador': nombre}        
+    return resultado
 
 if __name__ == '__main__':
     print("Ahora sí! buena suerte :)")
-    # A partir de aquí escriba el código que resuelve el enunciado
-    # Leer el enunciado con atención y consultar cualquier duda
+    
+    print('Ingrese la cantidad de jugadores')
 
+    numero_jugadores = int(input())    
+
+    jugadores = [blackjack() for x in range(numero_jugadores)]
+
+    jugadores.sort(reverse=True, key=lambda x: x['puntaje'])
+
+    [print ('*' ,end='') for x in range(45)]
+    print('')
+    print('Ranking!! :D')
+    [print ('*' ,end='') for x in range(45)]
+    print('')
+    [print('jugador >', x.get('jugador'),'puntaje:', x.get('puntaje')) for x in jugadores]
+    [print ('*' ,end='') for x in range(45)]
+    print('')
+    ranking = [x.get('puntaje') for x in jugadores]
+
+    if numero_jugadores > 1:
+
+        if ranking[1] == ranking[0]:
+            print('Empate!')
+
+        elif sum(ranking) == 0:
+            print('Perdedores!')
+
+        else:
+            winner = [x.get("jugador") for x in jugadores if x.get('puntaje') == ranking[0]]
+            print('Ganó', winner[0], '!!')
+
+    elif numero_jugadores == 0:
+        print('Volvé otro día entonces...')
+
+    else:
+        if ranking[0] > 0:
+           player = [x.get("jugador") for x in jugadores if x.get('puntaje') == ranking[0]]
+           print('Ganaste', player[0], '!!')
+
+        else:
+           player = [x.get("jugador") for x in jugadores if x.get('puntaje') == ranking[0]]
+           print('Perdiste', player[0], '!!')
 
     print("terminamos")
